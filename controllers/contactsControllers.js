f95aa2e98dbe00d07a1c5a6a892aa07ceedb9f6a
Oleg Kovriga <olegkovrigadev@gmail.com>
2024-01-24 23:52:15 +0200

    Update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -16,22 +16,22 @@ export const getOneContact = async (req, res) => {
   const { id } = req.params;
   const result = await getContactById(id);
 
-  if (!result) {
+  if (result) {
+    res.status(200).json(result);
+  } else {
     res.status(404).json({ message: 'Not found' });
   }
-
-  res.status(200).json(result);
 };
 
 export const deleteContact = async (req, res) => {
   const { id } = req.params;
   const result = await removeContact(id);
 
-  if (!result) {
+  if (result) {
+    res.status(200).json(result);
+  } else {
     res.status(404).json({ message: 'Not found' });
   }
-
-  res.status(200).json(result);
 };
 
 export const createContact = async (req, res) => {
@@ -63,9 +63,9 @@ export const updateContact = async (req, res) => {
 
   const result = await updateById(id, req.body);
 
-  if (!result) {
+  if (result) {
+    res.status(200).json(result);
+  } else {
     res.status(404).json({ message: 'Not found' });
   }
-
-  res.status(200).json(result);
 };
