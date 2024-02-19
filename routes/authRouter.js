d4e5b540c97159413c265fe85fad1f990e98d824
Oleg Kovriga <olegkovrigadev@gmail.com>
2024-02-19 23:59:05 +0200

    Add Send-Mail

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,18 +2,26 @@ import express from 'express';
 import validateBody from '../helpers/validateBody.js';
 import authenticate from '../helpers/authenticate.js';
 import upload from '../helpers/upload.js';
-import { registerSchema, loginSchema } from '../schemas/userSchemas.js';
+import {
+  registerSchema,
+  loginSchema,
+  emailSchema,
+} from '../schemas/userSchemas.js';
 import {
   register,
   login,
   getCurrent,
   logout,
   updateAvatar,
+  verifyEmail,
+  resendVerifyEmail,
 } from '../controllers/authControllers.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', validateBody(registerSchema), register);
+authRouter.get('/verify/:verificationCode', verifyEmail);
+authRouter.post('/verify', validateBody(emailSchema), resendVerifyEmail);
 authRouter.post('/login', validateBody(loginSchema), login);
 authRouter.get('/current', authenticate, getCurrent);
 authRouter.post('/logout', authenticate, logout);
